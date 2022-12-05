3fc79d3803f18e3dcc84bf090564e76a6b5f59b2
SouvikLaskar <souviklaskar13@gmail.com>
2022-12-05 10:20:19 +0530

    4th commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Name } from './shared/data.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AwardRecognition';
+  searchText: any;
+
+  Data: Name[]=[
+    new Name('GSI GI 815', 'Souvik', 'India','14-10-2022','https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
+    new Name('GSI GR 816','Siddhesh' , 'USA','05-18-2022', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
+    new Name('GSI GI 215','Rohan' ,'UK','03-11-2021','https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
+    new Name('GSI GM 519', 'Vikash' , 'Canada','11-02-2022', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
+    new Name('GSI GS 810','Sumukh' ,  'India','13-05-2022', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
+    new Name('GSI GJ 205', 'Tanmay' ,  'China','03-03-2021', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
+    new Name('GSI GI 852','Sayan' , 'Germany','25-12-2021','https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
+    new Name( 'GSI GG 258', 'Amit' , 'Hong Kong','26-08-2021', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
+    new Name('GSI GL 789', 'Sourav' ,'India','14-10-2022','https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
+    new Name('GSI GK 965', 'Abhinav' , 'Sri Lanka','17-11-2021', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+  ];
+  
+  onDataAdded(Data:Name){
+    this.Data.push(Data);
+
+  }
 }
--- src/app/nsearch/nsearch.component.ts
+++ src/app/nsearch/nsearch.component.ts
@@ -1,5 +1,6 @@
 import { Call } from '@angular/compiler';
-import { Component, Input, ViewChild } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
+import { Name } from '../shared/data.model';
 
 
 @Component({
@@ -8,21 +9,27 @@ import { Component, Input, ViewChild } from '@angular/core';
   styleUrls: ['./nsearch.component.css']
 })
 export class NsearchComponent {
-  searchText: any;
-  Names = [
-    { id: 'GSI GI 815', name: 'Souvik', country: 'India',doj:'14-10-2022', img:'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'},
-    { id: 'GSI GR 816', name: 'Siddhesh' , country: 'USA',doj:'05-18-2022', img:'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'},
-    { id: 'GSI GI 215', name: 'Rohan' , country: 'UK',doj:'03-11-2021', img:'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'},
-    { id: 'GSI GM 519', name: 'Vikash' , country: 'Canada',doj:'11-02-2022', img:'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg' },
-    { id: 'GSI GS 810', name: 'Sumukh' , country: 'India',doj:'13-05-2022', img:'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'},
-    { id: 'GSI GJ 205', name: 'Tanmay' , country: 'China',doj:'03-03-2021', img:'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'},
-    { id: 'GSI GI 852', name: 'Sayan' , country: 'Germany',doj:'25-12-2021', img:'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'},
-    { id: 'GSI GG 258', name: 'Amit' , country: 'Hong Kong',doj:'26-08-2021', img:'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'},
-    { id: 'GSI GL 789', name: 'Sourav' , country: 'India',doj:'14-10-2022', img:'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'},
-    { id: 'GSI GK 965', name: 'Abhinav' , country: 'Sri Lanka',doj:'17-11-2021', img:'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'}
-  ];
+  @ViewChild('idInput') idInputRef!: ElementRef;
+  @ViewChild('nameInput') nameInputRef!: ElementRef;
+  @ViewChild('countryInput') countryInputRef!: ElementRef;
+  @ViewChild('dojInput') dojInputRef!: ElementRef;
+  @ViewChild('imgInput') imgInputRef!: ElementRef;
+  
+  @Output() dataAdded = new EventEmitter<Name>();
 
-  onClick(){
+  
+  
+  onAddItem(){
+    const dataid=this.idInputRef.nativeElement.value;
+    const dataname=this.nameInputRef.nativeElement.value;
+    const datacountry=this.countryInputRef.nativeElement.value;
+    const datadoj=this.dojInputRef.nativeElement.value;
+    const dataimg=this.idInputRef.nativeElement.value;
+    const newdataAdded= new Name(dataid,dataname,datacountry,datadoj,dataimg);
+    this.dataAdded.emit(newdataAdded);
     
+
   }
+
+  
 }
